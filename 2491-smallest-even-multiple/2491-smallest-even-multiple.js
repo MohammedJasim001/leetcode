6c47ac05f5b14830c29e2c03dba929cc5f97d379
Mohammed Jasim <jasimat001@gmail.com>
2024-09-04 10:31:34 +0530

    Time: 63 ms (5.35%), Space: 48.4 MB (85.12%) - LeetHub

--- 2491-smallest-even-multiple/2491-smallest-even-multiple.js
+++ 2491-smallest-even-multiple/2491-smallest-even-multiple.js
@@ -3,11 +3,11 @@
  * @return {number}
  */
 var smallestEvenMultiple = function(n) {
-      for(i=1;i<=n;i++){
+      
        if(n%2!=0){
            return n*2
        }else{
            return n
        }
-   }
+   
 };
