5aed87df3f60b63fefe9146b4f26a2fa918bbca2
Mohammed Jasim <jasimat001@gmail.com>
2025-02-10 19:11:15 +0530

    Time: 0 ms (100%), Space: 48.9 MB (58.82%) - LeetHub

---
+++ 2955-account-balance-after-rounded-purchase/2955-account-balance-after-rounded-purchase.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number} purchaseAmount
+ * @return {number}
+ */
+var accountBalanceAfterPurchase = function(purchaseAmount) {
+      a = Math.floor((purchaseAmount+5)/10)*10
+    return 100-a
+};
