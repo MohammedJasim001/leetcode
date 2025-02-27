44d02bdaefd8b63ab8d5c8b16cce93dd7c6e72a7
Mohammed Jasim <jasimat001@gmail.com>
2025-02-27 18:38:01 +0530

    Time: 20 ms (36.65%), Space: 65.8 MB (13.53%) - LeetHub

---
+++ 2836-neither-minimum-nor-maximum/2836-neither-minimum-nor-maximum.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findNonMinOrMax = function(nums) {
+        if(nums.length<=2){
+        return -1
+    }
+    
+    return nums.sort((a,b)=>a-b)[1]
+};
