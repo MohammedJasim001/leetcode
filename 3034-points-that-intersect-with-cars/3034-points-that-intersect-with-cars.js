9d06425bcd8c6799902bb73155345bd761bd49df
Mohammed Jasim <jasimat001@gmail.com>
2025-03-10 15:47:01 +0530

    Time: 6 ms (38.3%), Space: 58.8 MB (8.51%) - LeetHub

--- 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.js
+++ 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.js
@@ -2,14 +2,13 @@
  * @param {number[][]} nums
  * @return {number}
  */
-var numberOfPoints = function(nums) {
-         nums.sort()
+var numberOfPoints = function (nums) {
     total = 0
     c = []
-    for(i=0;i<nums.length;i++){
-       for(j=nums[i][1];j>=nums[i][0];j--){
+    for (i = 0; i < nums.length; i++) {
+        for (j = nums[i][1]; j >= nums[i][0]; j--) {
             c.push(j)
-       }
+        }
     }
     return [...new Set(c)].length
 };
