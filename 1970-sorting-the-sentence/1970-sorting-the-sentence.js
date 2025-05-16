00c3336dae24e18745f96da4c1dd8386af250dab
Mohammed Jasim <jasimat001@gmail.com>
2025-05-16 11:15:35 +0530

    Time: 50 ms (10.2%), Space: 52.8 MB (92.29%) - LeetHub

--- 1970-sorting-the-sentence/1970-sorting-the-sentence.js
+++ 1970-sorting-the-sentence/1970-sorting-the-sentence.js
@@ -3,8 +3,8 @@
  * @return {string}
  */
 var sortSentence = function (s) {
-    arr = Array(s.split(' ').length).fill(false)
     s = s.split(' ')
+    arr = Array(s.length).fill(false)
     for (i = 0; i < s.length; i++) {
         arr[s[i][s[i].length - 1] - 1] = s[i].slice(0, s[i].length - 1)
     }
