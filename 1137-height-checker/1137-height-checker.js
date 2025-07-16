9eca07ab7a68bb560df25efd83876144bea63dfb
Mohammed Jasim <jasimat001@gmail.com>
2025-07-16 10:55:54 +0530

    Time: 2 ms (32.87%), Space: 54.4 MB (38.9%) - LeetHub

---
+++ 1137-height-checker/1137-height-checker.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var heightChecker = function (heights) {
+    count = 0
+    c = [...heights]
+    heights.sort((a, b) => a - b)
+    for (i = 0; i < heights.length; i++) {
+        c.push(heights[i])
+        if (c[i] !== heights[i]) {
+            count++
+        }
+    }
+    return count
+};
