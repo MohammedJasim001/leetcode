a03e00b69b6adaff93b07c97824dac4350c1fb60
Mohammed Jasim <jasimat001@gmail.com>
2025-09-25 11:23:53 +0530

    Time: 16 ms (19.29%), Space: 62.2 MB (23.64%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.js
+++ 0006-zigzag-conversion/0006-zigzag-conversion.js
@@ -7,14 +7,14 @@ var convert = function (s, numRows) {
     if (numRows === 1 || s.length <= numRows) return s;
     const result = Array.from({ length: numRows }, () => [])
     let row = 0
-    let goingDown = false
+    let down = false
     for (let char of s) {
         result[row].push(char);
         if (row === 0 || row === numRows - 1) {
-            goingDown = !goingDown;
+            down = !down;
         }
 
-        row += goingDown ? 1 : -1;
+        row += down ? 1 : -1;
     }
     return result.map((ele) => ele.join('')).join('')
 };
