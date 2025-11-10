b6f69af413a1370fa83bcd8a3058462f57d04808
Mohammed Jasim <jasimat001@gmail.com>
2025-11-10 10:48:49 +0530

    Time: 3 ms (66.54%), Space: 57.1 MB (85.4%) - LeetHub

--- 4107-find-missing-elements/4107-find-missing-elements.js
+++ 4107-find-missing-elements/4107-find-missing-elements.js
@@ -2,18 +2,18 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var findMissingElements = function(nums) {
-        nums.sort((a,b)=>a-b)
-    range = nums[nums.length-1]-nums[0]+1
-      let currVal = nums[0]
-      c = []
-   for(i=0;i<range;i++){
-       if(nums.includes(currVal)){
-           currVal++
-       }else{
-           c.push(currVal)
-           currVal ++
-       }
-   }
-   return c
+var findMissingElements = function (nums) {
+    nums.sort((a, b) => a - b)
+    range = nums[nums.length - 1] - nums[0] 
+    let currVal = nums[0]
+    c = []
+    for (i = 0; i < range; i++) {
+        if (nums.includes(currVal)) {
+            currVal++
+        } else {
+            c.push(currVal)
+            currVal++
+        }
+    }
+    return c
 };
