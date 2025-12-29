2d71fd6943890b6b2b9dbd7aa70a7684379b86cf
Mohammed Jasim <jasimat001@gmail.com>
2025-12-29 10:25:34 +0530

    Time: 2 ms (23.64%), Space: 55.2 MB (92.01%) - LeetHub

--- 3726-remove-zeros-in-decimal-representation/3726-remove-zeros-in-decimal-representation.js
+++ 3726-remove-zeros-in-decimal-representation/3726-remove-zeros-in-decimal-representation.js
@@ -6,7 +6,7 @@ var removeZeros = function (n) {
     n = n.toString()
     c = ''
     for (i = 0; i < n.length; i++) {
-        if (n[i] != 0) {
+        if (n[i] !== "0") {
             c += n[i]
         }
     }
