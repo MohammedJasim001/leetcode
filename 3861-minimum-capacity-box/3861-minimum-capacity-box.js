46445df5f9abf000b5ef438e7a9788581bc5b0ce
Mohammed Jasim <jasimat001@gmail.com>
2026-03-10 10:50:49 +0530

    Time: 2 ms (6.27%), Space: 55 MB (94.9%) - LeetHub

---
+++ 3861-minimum-capacity-box/3861-minimum-capacity-box.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} capacity
+ * @param {number} itemSize
+ * @return {number}
+ */
+var minimumIndex = function (capacity, itemSize) {
+    const map = {}
+    for (i = 0; i < capacity.length; i++) {
+        if (capacity[i] >= itemSize) {
+            map[i] = capacity[i]
+        }
+    }
+    let minValue = Infinity
+    let minIndex = -1
+    for (x in map) {
+        if (map[x] < minValue) {
+            minValue = map[x]
+            minIndex = Number(x)
+        }
+    }
+    return minIndex
+};
