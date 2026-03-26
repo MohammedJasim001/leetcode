68dda722564922fbd426e908531dcc9115c19676
Mohammed Jasim <jasimat001@gmail.com>
2026-03-26 11:32:12 +0530

    Time: 0 ms (100%), Space: 52.9 MB (94.95%) - LeetHub

--- 0877-stone-game/0877-stone-game.js
+++ 0877-stone-game/0877-stone-game.js
@@ -3,29 +3,5 @@
  * @return {boolean}
  */
 var stoneGame = function (piles) {
-    let alice = 0;
-    let bob = 0;
-    let turn = 0;
-
-    while (piles.length > 0) {
-        let left = piles[0];
-        let right = piles[piles.length - 1];
-
-        let pick;
-        if (left > right) {
-            pick = piles.shift();
-        } else {
-            pick = piles.pop();
-        }
-
-        if (turn === 0) {
-            alice += pick;
-        } else {
-            bob += pick;
-        }
-
-        turn = 1 - turn;
-    }
-
     return  true
 };
