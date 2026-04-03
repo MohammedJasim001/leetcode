d2958f459c8b026b6538e10892624d6b2493971e
Mohammed Jasim <jasimat001@gmail.com>
2026-04-03 10:28:44 +0530

    Time: 0 ms (100%), Space: 56.3 MB (24.53%) - LeetHub

---
+++ 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstMatchingIndex = function (s) {
+    for (i = 0; i < s.length; i++) {
+        if (s[i] === s[s.length - (i + 1)]) {
+            return i
+        }
+    }
+    return -1
+};
