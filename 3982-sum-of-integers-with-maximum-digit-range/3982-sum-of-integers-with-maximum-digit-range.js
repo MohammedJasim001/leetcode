d558895d480885e4a75bfe7ead8afe8e3ad35039
Mohammed Jasim <jasimat001@gmail.com>
2026-07-10 12:27:37 +0530

    Time: 1 ms (96%), Space: 56.2 MB (70.67%) - LeetHub

--- 3982-sum-of-integers-with-maximum-digit-range/3982-sum-of-integers-with-maximum-digit-range.js
+++ 3982-sum-of-integers-with-maximum-digit-range/3982-sum-of-integers-with-maximum-digit-range.js
@@ -5,41 +5,29 @@
 var maxDigitRange = function (nums) {
     let maxRange = 0
     let result = 0
-    for (i = 0; i < nums.length; i++) {
+    for(i=0;i<nums.length;i++){
         let num = nums[i]
         let largest = 0
         let lowest = Infinity
         let range = 0
-        while (num > 0) {
-            const digit = num % 10
-            if (digit > largest) {
+        while (num>0){
+            const digit = num%10
+            if(digit>largest){
                 largest = digit
             }
-            if (digit < lowest) {
+            if(digit<lowest){
                 lowest = digit
             }
-            num = Math.floor(num / 10)
+            num = Math.floor(num/10)
         }
         range = largest - lowest
-        if (range > maxRange) {
+        if(range > maxRange){
+            result = nums[i]
             maxRange = range
+        }else if(range === maxRange){
+            result+=nums[i]
         }
     }
-    for (let i = 0; i < nums.length; i++) {
-        let num = nums[i];
-        let largest = 0;
-        let lowest = 9;
-
-        while (num > 0) {
-            let digit = num % 10;
-            largest = Math.max(largest, digit);
-            lowest = Math.min(lowest, digit);
-            num = Math.floor(num / 10);
-        }
-
-        if (largest - lowest === maxRange) {
-            result += nums[i];
-        }
-    }
+    
     return result
 }
