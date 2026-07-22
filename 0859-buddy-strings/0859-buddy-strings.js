6161bef2f011d1434a56c305cb0a8b43852bf77a
Mohammed Jasim <jasimat001@gmail.com>
2026-07-22 17:54:42 +0530

    Time: 2 ms (61.05%), Space: 56.6 MB (35.79%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+var buddyStrings = function (s, goal) {
+
+    if (s.length !== goal.length) return false;
+
+    if (s === goal) {
+        const set = new Set(s);
+        console.log(set, 'ssss')
+        return set.size < s.length;
+    }
+
+
+    const diff = [];
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] !== goal[i]) {
+            diff.push(i);
+
+            if (diff.length > 2) return false;
+        }
+    }
+    return (
+        diff.length === 2 &&
+        s[diff[0]] === goal[diff[1]] &&
+        s[diff[1]] === goal[diff[0]]
+    );
+};
