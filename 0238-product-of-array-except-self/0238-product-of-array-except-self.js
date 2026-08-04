f983f8ce4e3a910e2f238e3a6ff60c3eb5f85c99
Mohammed Jasim <jasimat001@gmail.com>
2026-08-04 16:07:45 +0530

    Time: 2 ms (96.73%), Space: 71.8 MB (33.21%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function (nums) {
+    const n = nums.length
+    const result = new Array(n).fill(0)
+    let prefix = 1
+    for (let i = 0; i < nums.length; i++) {
+        result[i] = prefix
+        prefix *= nums[i]
+    }
+    let suffix = 1;
+    for (let i = n - 1; i >= 0; i--) {
+        result[i] *= suffix;
+        suffix *= nums[i];
+    }
+    return result
+};
